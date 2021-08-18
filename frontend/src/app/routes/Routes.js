04f34546e9b11ea3fcdf311e4f8a1510be97bcf5
Deofino <guilhermedelfino25@gmail.com>
2021-08-17 22:05:01 -0300

    :recycle: Add backend code and more

--- frontend/src/app/routes/Routes.js
+++ frontend/src/app/routes/Routes.js
@@ -3,10 +3,19 @@ import React from 'react';
 // Components 
 import Main from '../components/Main/Main';
 import Form from '../components/Form/Form';
-
 import { Route } from 'react-router-dom';
 
 export default function Routes() {
+
+    React.useEffect(() => {
+
+        (async () => {
+            let req = await fetch(`http://${window.location.hostname}/FicaFacil/backend/api/`);
+            let res = await req.text();
+
+            console.log(res);
+        })();
+    }, []);
 
     return (
         <>
@@ -14,7 +23,7 @@ export default function Routes() {
                 <Main>
                     <Form />
                     <div className='l-main__image'>
-                        <img src={__dirname + 'img/svg/svg-login.svg'} alt='Mulher usando celular, cadeado, seguranca' />
+                        <img src={ __dirname + 'img/svg/svg-login.svg' } alt='Mulher usando celular, cadeado, seguranca' />
                     </div>
                 </Main>
             </Route>
@@ -37,5 +46,5 @@ export default function Routes() {
                 </Main>
             </Route>
         </>
-    )
+    );
 }
