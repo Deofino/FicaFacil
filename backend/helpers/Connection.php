4e1cee6de8b86cb8b88d061e00d7f72ede806ee6
Guilherme Delfino <guilhermedelfino25@gmail.com>
2021-08-27 11:18:49 -0300

    :construction: Class Connection with PDO
    In progress class, no test still

--- backend/helpers/Connection.php
+++ backend/helpers/Connection.php
@@ -3,14 +3,22 @@
     namespace Helper;
 
 use PDO;
+use Helper\Response;
+abstract class Connection{
 
-class Connection{
-
-        private $connection=null;
+        private static $connection=null;
+        
         public static function getConn(): PDO{
-            $dns=DB_DRIVER."";
-            $user='';
-            $password='';
-
+            $dns=DB_DRIVER.":host". DB_DATABASE_HOST. ";dbname=".DB_DATABASE_NAME;
+            $user=DB_DATABASE_USER;
+            $password=DB_DATABASE_PASSWORD;
+            if(self::$connection !== null){
+                try {
+                    self::$connection = new PDO($dns,$user,$password); 
+                } catch (\Throwable $th) {
+                    echo Response::error($th);
+                }
+            }
+            return self::$connection;
         }
     }
