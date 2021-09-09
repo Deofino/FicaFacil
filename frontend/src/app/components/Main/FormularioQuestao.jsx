bd2747b2d45847447ff6adc2913d67316c288264
Kauê Loviz de Oliveira <56116607+kauezin007@users.noreply.github.com>
2021-09-08 21:07:18 -0300

    :construction: testa ai now
    area materia

--- frontend/src/app/components/Main/FormularioQuestao.jsx
+++ frontend/src/app/components/Main/FormularioQuestao.jsx
@@ -1,22 +1,29 @@
 import React, { useState, Fragment, useRef, useEffect } from "react";
 import axios from 'axios';
 import { Input, Button, Select, MenuItem } from '../Form';
-import { FaCalendar, FaUser, FaImages, FaUniversity, FaBookOpen, FaOptinMonster, FaXbox, FaListAlt } from 'react-icons/fa';
+import { FaCalendar, FaUser, FaImages, FaUniversity, FaBookOpen, FaListAlt } from 'react-icons/fa';
 
 export default function FormularioQuestao() {
     const [ alternativas, setAlternativas ] = useState([]);
     const [ selectUniversidade, setselectUniversidade ] = useState(0);
+    const [ selectAreaMateria, setselectAreaMateria ] = useState(0);
     const refSelectUniversidade = useRef(null);
+    const refSelectAreaMateria = useRef(null);
     const [ universidades, setUniversidades ] = useState(null);
+    const [ areamaterias, setAreaMaterias ] = useState(null);
     useEffect(() => {
         axios.get(process.env.REACT_APP_API + '/universidade/index/')
             .then(data => setUniversidades(data.data))
             .catch(error => console.error(error));
+        axios.get(process.env.REACT_APP_API + '/areamateria/index/')  
+        .then(data => setAreaMaterias(data.data))
+        .catch(error => console.error(error));  
     }, []);
 
 
     const [ inputAlternativa, setInputAlternativa ] = useState('');
     const refUniversidade = useRef(null);
+    const refAreaMateria = useRef(null);
     function submitForm(e) {
         e.preventDefault();
         let formulario = document.getElementById('form');
@@ -42,6 +49,18 @@ export default function FormularioQuestao() {
                     <MenuItem value={ 0 }>Selecione</MenuItem>
                     { universidades && universidades.data.map((el, i) =>
                         <MenuItem key={ i } value={ el[ 'idUniversidade' ] }>{ el[ 'nomeUniversidade' ] }</MenuItem>
+                    ) }
+                </Select>
+                <Select label='Area Matéria' id='areamateria' name='areamateria' ref={ refSelectAreaMateria }
+                    onChange={ e => {
+                        // console.log(e.target);
+                        setselectAreaMateria(e.target.value);
+                    } }
+                    value={ selectAreaMateria }
+                >
+                    <MenuItem value={ 0 }>Selecione</MenuItem>
+                    { areamaterias && areamaterias.data.map((el, i) =>
+                        <MenuItem key={ i } value={ el[ 'idAreamateria' ] }>{ el[ 'nomeAreaMateria' ] }</MenuItem>
                     ) }
                 </Select>
                 <Input title='Alternativa' value={ inputAlternativa } icon={ <FaListAlt /> } onChange={ ({ target }) => {
@@ -72,8 +91,16 @@ export default function FormularioQuestao() {
             </Fragment>
 
             <Fragment>
-                <form method="post" id='formAM' onSubmit={ (e) => submitForm(e) }>
-                    <Input title='Area Matéria:' id='areamateria' name='areamateria' type='text' icon={ <FaBookOpen /> } inputMode='text' />
+                <form method="post" id='formAM' onSubmit={ (e) => {
+                    e.preventDefault();
+                    axios.post(process.env.REACT_APP_API + '/areamateria/create/',
+                        JSON.stringify({
+                            areamateria: refAreaMateria.current.value
+                        }))
+                        .then(data => console.log(data));
+
+                } }>
+                    <Input title='Area Matéria:' ref={ refAreaMateria } id='areamateria' name='areamateria' type='text' icon={ <FaBookOpen /> } inputMode='text' />
                     <Button type='button' styleButton={ { marginTop: 20 } } onClick={ () => {
 
                     } }>Adicionar Area Matéria</Button>
