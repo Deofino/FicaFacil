37d50a46f09a988dede2765f8ce0b5c0863e8966
Deofino <guilhermedelfino25@gmail.com>
2021-09-23 11:10:20 -0300

    :fire: Front end Completo no update da Universidade
    Peguem de base essa universidade e apliquem nos outros para um CRUD completo

--- frontend/src/app/components/Main/FormularioUniversidade.jsx
+++ frontend/src/app/components/Main/FormularioUniversidade.jsx
@@ -1,16 +1,92 @@
-import React, { useState, Fragment, useRef, createElement } from "react";
+import React, { useState, useRef } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 import { AlertSuccess } from "../Alert/Modal";
 import { Input, Button, Table } from "../Form";
-import { FaBookOpen } from "react-icons/fa";
+import { FaBookOpen, FaTimes } from "react-icons/fa";
+import { Tooltip, IconButton } from "@material-ui/core";
+import { ToastError, ToastSuccess } from "../Alert/Toast";
+
+const Backdrop = (props) => {
+  const [attUniversidade, setAttUniversidade] = useState(props.data[1] || ""); // State para atualizar o campo
+  const [errAttUniversidade, seterrAttUniversidade] = useState(null); // State para atualizar o campo
+  const updateEvent = (e) => { // na hora que clica no botao de atualizar
+    e.preventDefault();
+    if (
+      attUniversidade !== null &&
+      attUniversidade !== "" &&
+      attUniversidade.length > 3
+    ) { // verificacao dos campos
+      axios
+        .post(
+          `${process.env.REACT_APP_API}/universidade/update/`, // requisicao post backend/api/campo/update METHOD POST
+          JSON.stringify({ // faz um json com 
+            universidade: attUniversidade, // o campo que deve ser atualizado
+            id: props.data[0], // o id da universidade que deve ser atualizado no WHERE
+          })
+        )
+        .then((value) => {
+          if (value.data.status_code) { // verifica se status code retorna 200 = OK
+            ToastSuccess({ text: value.data.data }); // mensagem de sucesso
+            close() // fecha o backdrop
+            setTimeout(() => {
+              window.location.reload(); // atualiza a pagina dps de 4 segundos
+            }, 4000);
+          }
+        })
+        .catch((error) => ToastError({text: error})); // caso backend retorne erro aparece aqui
+    } else { // previne e coloca os erros 
+      seterrAttUniversidade("O campo tem que ter no minimo 3 caracteres");
+    }
+  };
+  const close = () => {
+    let backdrop = document.querySelector("#backdrop");
+    backdrop.classList.toggle("open");
+    ReactDOM.unmountComponentAtNode(backdrop);
+  };
+  return (
+    <section className="c-formularioUpdate" id="c-formularioUpdate">
+      <Tooltip
+        className="c-formularioUpdate__close"
+        title="Fechar"
+        enterDelay={400}
+        enterNextDelay={200}
+      >
+        <IconButton onClick={() => close()}>
+          <FaTimes />
+        </IconButton>
+      </Tooltip>
+      <h1 className="c-formularioUpdate__headline">
+        Atualizar Universidade: {props.data[1]}
+      </h1>
+      <form
+        onSubmit={(e) => updateEvent(e)}
+        encType="multipart/form-data"
+        className="c-formularioUpdate__form"
+        id="formUpdate"
+      >
+        <Input
+          title={props.titles[1].headerName || "Input"}
+          id={props.titles[1].field || null}
+          className="c-formularioUpdate__item"
+          name={props.titles[1].field || null}
+          type={props.titles[1].type || "text"}
+          value={attUniversidade}
+          error={errAttUniversidade}
+          onChange={(e) => {
+            setAttUniversidade(e.target.value);
+          }}
+          inputMode="text"
+        />
+        <Button type="submit">Atualizar</Button>
+      </form>
+    </section>
+  );
+};
 
 export default function FormularioUniversidade() {
   const [ErroUniversidade, setErroUniversidade] = useState(null);
   const refUniversidade = useRef(null);
-
-  const [attUniversidade, setAttUniversidade] = React.useState(""); // State para atualizar o campo 
-
 
   const [universidades, setUniversidades] = React.useState([]);
   React.useEffect(() => {
@@ -56,40 +132,10 @@ export default function FormularioUniversidade() {
     let titles = colunas;
     data = Object.values(data);
 
-
-
-    const Back = ({ titles, data }) => {
-      return (
-        <section className="c-formularioUpdate" id="c-formularioUpdate">
-          <form
-            onSubmit={() => {}}
-            encType="multipart/form-data"
-            id="formUpdate"
-          >
-            <Input
-              title={titles[1].headerName || "Input"}
-              id={titles[1].field || null}
-              name={titles[1].field || null}
-              type={titles[1].type || "text"}
-              value={data[1]}
-              onChange={(e) => {
-                console.log(e);
-              }}
-              inputMode="text"
-            />
-            )<Button type="submit">Atualizar</Button>
-          </form>
-        </section>
-      );
-    };
-
     let div = document.querySelector("#backdrop");
-    if (div.hasChildNodes) {
-      // div.removeChild(div.firstChild);
-      console.log(div);
-    }
+    div.classList.toggle("open");
 
-    ReactDOM.render(<Back data={data} titles={titles} />, div);
+    ReactDOM.render(<Backdrop data={data} titles={titles} />, div);
   };
 
   return (
@@ -97,6 +143,7 @@ export default function FormularioUniversidade() {
       <form
         method="post"
         id="formU"
+        className="c-form"
         onSubmit={(e) => {
           e.preventDefault();
 
@@ -133,6 +180,7 @@ export default function FormularioUniversidade() {
           ref={refUniversidade}
           error={ErroUniversidade}
           id="universidade"
+          class=" c-form__item"
           name="universidade"
           type="text"
           icon={<FaBookOpen />}
