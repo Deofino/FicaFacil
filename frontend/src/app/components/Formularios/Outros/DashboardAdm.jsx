d51d7fdd3356d887e50ee60d739845a591c9db3a
Vitória <56117556+vitoriaGoncalves08@users.noreply.github.com>
2021-11-10 18:24:54 -0300

    :fire: começando dashboard

--- frontend/src/app/components/Formularios/Outros/DashboardAdm.jsx
+++ frontend/src/app/components/Formularios/Outros/DashboardAdm.jsx
@@ -1,41 +1,38 @@
-import React, { Fragment } from "react";
-import reactDOM from "react-dom";
-import { Button, } from "../../Form";
-import Cookie from "../../../../img/project/cookie.png";
+import React, { } from "react";
 
 export default function DashboardAdm() {
     return (
-        <section className="dashboardAdm">
-            <div className="dashboardAdm__title">
-                <h1>Métricas</h1>
-            </div>
-            <div className="dasboardAdm__left">
+        <section className="dashboard">
+            <h1 className="dashboard__title">Métricas</h1>
+            <div className="dashboard__c">
+                <div className="dashboard__c__left">
+                    <div className="dashboard__c__left__um">
+                        <h3 className="dashboard__c__left__um__title">Total de Simulados</h3>
+                    </div>
+                    <div className="dashboard__c__left__dois">
+                        <h3 className="dashboard__c__left__dois__title">Entradas Mensais</h3>
+                    </div>
+                    <div className="dashboard__c__left__et">
+                        <div className="dashboard__c__left__et__tres">
 
-                <div className="dasboardAdm__left__totalSimulados">
-
+                        </div>
+                        <div className="dashboard__c__left__et__quatro">
+                            <h3 className="dashboard__c__left__quatro__title">Taxa de acertos</h3>
+                        </div>
+                    </div>
                 </div>
-                <div className="dasboardAdm__left__entradas">
+                <div className="dashboard__c__right">
 
+                    <div className="dashboard__c__right__profile">
+
+                    </div>
+                    <div className="dashboard__c__right__statics">
+
+                    </div>
+                    <div className="dashboard__c__right__statics">
+
+                    </div>
                 </div>
-                <div className="dasboardAdm__left__entradaTotal">
-
-                </div>
-                <div className="dasboardAdm__left__taxaAcerto">
-
-                </div>
-            </div>
-            <div className="dasboardAdm__right">
-
-                <div className="dasboardAdm__right__profile">
-
-                </div>
-                <div className="dasboardAdm__right__bugs">
-
-                </div>
-                <div className="dasboardAdm__right__perma">
-
-                </div>
-
             </div>
         </section>
     );
